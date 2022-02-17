5483207d880fdc2330326af6a2e731dbdae7fcff
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-18 00:59:43 +0530

    Calculating the harmonic number using Java

--- src/CoreProblems/CoreProblems.java
+++ src/CoreProblems/CoreProblems.java
@@ -1,22 +1,28 @@
 package CoreProblems;
 import java.util.Scanner;
 public class CoreProblems {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);  
-      System.out.print(" Power of 2 problem using Java  ");
-  	  System.out.print("Enter a number: ");  
-  	  int num = sc.nextInt();
-  	  int m = 2;
-  	  int limit = m*num;
-  	  System.out.println("The power table is: ");
-  	  for(int p=1;p<=limit;)
-  	  {
-  		  if(num==0)
-  		  p=1;
-  		  else
-  		  p=p*2;
-  		  System.out.println(p);
-  	 }	  
-    }
+	// Function to find N-th Harmonic Number
+	static double nthHarmonic(int N)
+	{
+		// H1 = 1
+		float harmonic = 1;
 
+		// loop to apply the forumula
+		// Hn = H1 + H2 + H3 ... + Hn-1 + Hn-1 + 1/n
+		for (int i = 2; i <= N; i++) {
+			harmonic += (float)1 / i;
+		}
+
+		return harmonic;
+	}
+	
+
+	public static void main (String[] args) {
+	    Scanner sc = new Scanner(System.in);  
+	    System.out.print("Enter a number: ");  
+	    int N = sc.nextInt();
+		
+		System.out.print(nthHarmonic(N));
+		
+	}
 }
