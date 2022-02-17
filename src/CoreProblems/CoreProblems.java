002f8524c0d174d3597fb7105c21d4e0cac84224
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-17 20:20:26 +0530

    Leap Year Program using java

--- src/CoreProblems/CoreProblems.java
+++ src/CoreProblems/CoreProblems.java
@@ -1,7 +1,33 @@
 package CoreProblems;
-
+import java.util.Scanner;
 public class CoreProblems {
-	public static void main(String[] args) {
-      System.out.println("Working on Core Programs using java");
-	}
+    public static void main(String[] args) {
+        System.out.println("Working on Core Programs using java");
+        System.out.println("Leap Year Problem");
+
+        int year;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any Year:");
+        year = scan.nextInt();
+        scan.close();
+        boolean isLeap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
+            } else
+                isLeap = true;
+        } else {
+            isLeap = false;
+        }
+
+        if (isLeap == true)
+            System.out.println(year + " is a Leap Year.");
+        else
+            System.out.println(year + " is not a Leap Year.");
+    }
+
 }
