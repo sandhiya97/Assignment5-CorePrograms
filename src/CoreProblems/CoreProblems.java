ba701c45018426b273515e8a686355f20dea8163
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-17 20:30:05 +0530

    Power of 2 problem using Java

--- src/CoreProblems/CoreProblems.java
+++ src/CoreProblems/CoreProblems.java
@@ -2,32 +2,21 @@ package CoreProblems;
 import java.util.Scanner;
 public class CoreProblems {
     public static void main(String[] args) {
-        System.out.println("Working on Core Programs using java");
-        System.out.println("Leap Year Problem");
-
-        int year;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter any Year:");
-        year = scan.nextInt();
-        scan.close();
-        boolean isLeap = false;
-
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0)
-                    isLeap = true;
-                else
-                    isLeap = false;
-            } else
-                isLeap = true;
-        } else {
-            isLeap = false;
-        }
-
-        if (isLeap == true)
-            System.out.println(year + " is a Leap Year.");
-        else
-            System.out.println(year + " is not a Leap Year.");
+      Scanner sc = new Scanner(System.in);  
+      System.out.print(" Power of 2 problem using Java  ");
+  	  System.out.print("Enter a number: ");  
+  	  int num = sc.nextInt();
+  	  int m = 2;
+  	  int limit = m*num;
+  	  System.out.println("The power table is: ");
+  	  for(int p=1;p<=limit;)
+  	  {
+  		  if(num==0)
+  		  p=1;
+  		  else
+  		  p=p*2;
+  		  System.out.println(p);
+  	 }	  
     }
 
 }
