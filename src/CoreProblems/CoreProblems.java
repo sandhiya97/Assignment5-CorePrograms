7934590f59af44304efd7fb0558d81279eb2236d
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-18 01:51:00 +0530

    Checking whether the number is odd or Even using Java

--- src/CoreProblems/CoreProblems.java
+++ src/CoreProblems/CoreProblems.java
@@ -1,13 +1,13 @@
 package CoreProblems;
 import java.util.Scanner;
 public class CoreProblems {
-    // Swapping two numbers
+    // Odd or Even
 
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);  
-		System.out.print("Enter a number: ");  
-		int number = sc.nextInt();
-	    String msg =  (number % 2 == 0) ? "The number is even!" : "The number is odd!";
-	    System.out.println(msg);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        String msg = (number % 2 == 0) ? "The number is even!" : "The number is odd!";
+        System.out.println(msg);
     }
 }
