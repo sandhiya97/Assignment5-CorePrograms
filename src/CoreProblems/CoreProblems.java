2dd9d9b067efd1713fd37c8d8ea6f7560c802022
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-18 01:42:26 +0530

    Calculating the Quotient and Reminder using Java

--- src/CoreProblems/CoreProblems.java
+++ src/CoreProblems/CoreProblems.java
@@ -4,13 +4,13 @@ public class CoreProblems {
     // Calculating the Prime Factors
 
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);  
-  	  System.out.print("Enter the dividend: ");  
-  	  int dividend = sc.nextInt();
-  	  Scanner sc1 = new Scanner(System.in);  
-  	  System.out.print("Enter the divisor: ");  
-  	  int divisor = sc.nextInt();
-  	  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        int dividend = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+
         int quotient = dividend / divisor;
         int remainder = dividend % divisor;
 
